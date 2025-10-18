cbf495dbf96fc414630a93a94360c9a157bf5aba
vadlamudi rajesh <rajesh648n@gmail.com>
2025-10-18 15:01:36 +0530

    Initial commit

---
+++ src/main/java/com/walmart/nya/master/nyamaster/NyaMasterApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.nya.master.nyamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/nya/master/nyamaster/NyaMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.nya.master.nyamaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
